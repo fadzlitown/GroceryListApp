addfc4166ccf076db9ff6dbfee70de02f6394a7a
Fadzli Razali <fadzlitown@gmail.com>
2015-01-12 00:16:55 +0800

    Clean project

--- src/main/java/com/example/fadzlirazali/grocerylistapp/MainActivity.java
+++ src/main/java/com/example/fadzlirazali/grocerylistapp/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.fadzlirazali.grocerylistapp;
 
-import gitandroid.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
